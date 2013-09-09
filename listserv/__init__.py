1219605d66e32ab0a89812138e398acdcd5a1982
Ryan Bagwell <ryan@ryanbagwell.com>
2013-09-09 11:42:13 -0500

    moving lcmdx source to new source dir, other progress

--- listserv/__init__.py
+++ listserv/__init__.py
@@ -1,16 +1,28 @@
-import socket
+from subprocess import Popen, PIPE
 
-
-class Listsrv(object):
+class Listserv(object):
     host = None
     port = None
-    ip = None
     email = None
     password = None
-    conn = None
 
 
     def __init__(self, *args, **kwargs):
-        pass
+        self.host = kwargs.get('host', None)
+        self.port = kwargs.get('port', 2306)
+        self.email = kwargs.get('email', None)
+        self.password = kwargs.get('password', None)
+
+
+    def run(self, cmd):
+        """ Runs the supplied Listsrv command and returns the output """
+        print "lcmdx %s:%s %s %s %s" % (self.host, self.port, self.email, self.password, cmd)
+        x = Popen("lcmdx %s:%s %s %s %s" % (self.host, self.port, self.email, self.password, cmd), shell=True, stdout=PIPE).communicate()
+
+
+    def get_subscribers(self, list_name=None):
+        """ Returns subscribers for the given list """
+        subscribers = self.run("REVIEW %s MSG" % list_name)
+
 
 
